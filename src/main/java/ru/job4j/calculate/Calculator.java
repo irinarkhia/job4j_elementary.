a1c0aad41534abacacfed53cd7b17dc21e21c5a9
Irina <irinarkhia@yandex.ru>
2023-10-07 20:05:58 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/calculate/Calculator.java
+++ src/main/java/ru/job4j/calculate/Calculator.java
@@ -1,7 +1,6 @@
 package ru.job4j.calculate;
 
 public class Calculator {
-    @SuppressWarnings("checkstyle:WhitespaceAround")
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
