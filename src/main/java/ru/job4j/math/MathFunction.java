725d819c3b3e911c3f0901e76c0cfb9e688ba2bc
Irina <irinarkhia@yandex.ru>
2023-10-09 18:15:08 +0300

    1. Модульные тесты. [#159435 #451354]

---
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -0,0 +1,20 @@
+package ru.job4j.math;
+
+public class MathFunction {
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+
+    public static double sumAndMultiply(double first, double second) {
+        return ru.job4j.math.MathFunction.sum(first, second)
+                + ru.job4j.math.MathFunction.multiply(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+    }
+}
--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -1,12 +1,10 @@
 package ru.job4j.converter;
 
-
-import org.testng.annotations.Test;
-
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
-
 class ConverterTest {
+
     @Test
     void whenConvert140RblThen2Euro() {
         float in = 140;
@@ -17,8 +15,17 @@ class ConverterTest {
     }
 
     @Test
+    void whenConvert200RblThen3dot3333Dollar() {
+        float in = 200;
+        float expected = 3.3333F;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
     void whenConvert180RblThen3Dlr() {
-        float in = 180;
+        float in = 180F;
         float expected = 3;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
