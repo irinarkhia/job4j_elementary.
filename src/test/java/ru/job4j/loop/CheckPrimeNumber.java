3e024ba316d1e17af7c14a78a8028a9273d3066a
Irina <irinarkhia@yandex.ru>
2023-10-11 18:56:00 +0300

    5.6. Простые числа [#156314]

--- src/test/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/test/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean canDivByNum = false;
-        for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
+        for (int i = 2; i < number + 1; i++) {
+            if (number % i == 0 && number != i) {
                 break;
             }
             canDivByNum = true;
---
+++ src/test/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int count = 0;
+        for (int i = 2; i <= finish; i++) {
+            if (CheckPrimeNumber.check(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
