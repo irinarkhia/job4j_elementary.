ee59ec8d3f9c4c100004db69c7552692e23f8644
Irina <irinarkhia@yandex.ru>
2023-10-13 00:18:06 +0300

    5.9. Крест в псевдографике.  [#177182]

---
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,28 @@
+package ru.job4j.loop;
+
+public class Slash {
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                boolean left = row == cell; /* добавить условие для левой диагонали - с левого верхнего угла в правый нижний*/
+                boolean right = row + cell == size - 1; /* добавить условие для правой диагонали - из левого нижнего в правый верхний*/
+                if (left) {
+                    System.out.print("0");
+                } else if (right) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw by 3");
+        draw(3);
+        System.out.println("Draw by 5");
+        draw(5);
+    }
+}
