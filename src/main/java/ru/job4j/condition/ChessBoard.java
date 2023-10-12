0c73a033b0f2d1e7993a494238f5fe63d2ca44d6
Irina <irinarkhia@yandex.ru>
2023-10-12 18:04:16 +0300

    7. Расстояние между точками в системе координат [#188 #450928]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -6,7 +6,7 @@ public class ChessBoard {
         if (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)) {
             if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
-                rsl = Math.abs(x2 - x1);
+                rsl = Math.abs(x2  - x1);
             }
         }
         return rsl;
