de5c29d46d71bd699e76adce791b59f7a6117b1b
Irina <irinarkhia@yandex.ru>
2023-10-13 16:10:26 +0300

    6.6.2. Двухмерный массив. Циклы. [#306725]

---
+++ src/test/java/ru/job4j/array/MatrixSumTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MatrixSumTest {
+
+    @Test
+    public void whenSingle() {
+        int[][] array = {
+                {10}
+        };
+        int result = MatrixSum.sum(array);
+        int expected = 10;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenTwo() {
+        int[][] array = {
+                {1, 2},
+                {1, 2}
+        };
+        int result = MatrixSum.sum(array);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenThree() {
+        int[][] array = {
+                {1, 2, 1},
+                {1, 2, 0}
+        };
+        int result = MatrixSum.sum(array);
+        int expected = 7;
+        assertThat(result).isEqualTo(expected);
+    }
+}
