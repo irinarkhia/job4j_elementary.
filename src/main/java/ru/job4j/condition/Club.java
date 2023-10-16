d252831750fb7d805994d440d91587e5bb6aeec1
Irina <irinarkhia@yandex.ru>
2023-10-16 19:10:57 +0300

    4.3.2. Сокращенный оператор || [#333784 #451445]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Club {
     public static void permission(boolean hasMoney, boolean beFriend) {
-        if (hasMoney || beFriend) {
+        if (hasMoney  || beFriend) {
             System.out.println("I can go to the club.");
         } else {
             System.out.println("I can't.");
