1c5f65d7f4556f42f7337e086005d27b1153f460
Irina <irinarkhia@yandex.ru>
2023-10-16 12:03:24 +0300

    6.3. Массив заполнен true или false [#53857 #452383]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -1,21 +1,19 @@
 package ru.job4j.array;
 
-import java.util.Arrays;
-
-public class Machine {
-    public static int[] change(int money, int price) {
-        int[] coins = {10, 5, 2, 1};
-        int[] rsl = new int[100];
-        int size = 0;
-        int back = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (back > coins[i]) {
-                rsl[i] = coins[i];
-                back = back - coins[i];
-                size++;
-                }
-            break;
-            }
-        return Arrays.copyOf(rsl, size);
-    }
-}
+//public class Machine {
+   // public static int[] change(int money, int price) {
+//        int[] coins = {10, 5, 2, 1};
+//        int[] rsl = new int[100];
+//        int size = 0;
+//        int back = money - price;
+//        for (int i = 0; i < coins.length; i++) {
+//            while (back > coins[i]) {
+//                rsl[i] = coins[i];
+//                back = back - coins[i];
+//                size++;
+//                }
+//            break;
+//            }
+//        return Arrays.copyOf(rsl, size);
+    //}
+//}
