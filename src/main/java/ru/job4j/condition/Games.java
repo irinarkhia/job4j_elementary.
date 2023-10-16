f4d74c61fe5003e445df6438c87cde787b6fc5a6
Irina <irinarkhia@yandex.ru>
2023-10-16 19:10:16 +0300

    4.3.1. Сокращенный оператор && [#333783 #451443]

--- src/main/java/ru/job4j/condition/Games.java
+++ src/main/java/ru/job4j/condition/Games.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class Games {
     public static void permission(boolean allowByParent, boolean hasMoney) {
-        if (allowByParent && hasMoney) {
+        if (allowByParent  && hasMoney) {
             System.out.println("I can go to the computer club.");
         } else {
             System.out.println("I can't.");
