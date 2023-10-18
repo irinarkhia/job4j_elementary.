c37cc87b2845be757a236bf958962c62822fcd77
Irina <irinarkhia@yandex.ru>
2023-10-18 20:13:30 +0300

    3. Тесты для определения расстояния между точками. [#159459 #451380]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,11 +2,11 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        return  Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y2 - y1, 2));
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y2 - y1, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(3, 3, 6, 6);
+        double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0) " + result);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -18,12 +18,34 @@ class PointTest {
     }
 
     @Test
-    void when33to66then4() {
+    void when33to66then4dot2426() {
         double expected = 4.2426;
         int x1 = 3;
         int y1 = 3;
         int x2 = 6;
         int y2 = 6;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when30to60then3() {
+        double expected = 3;
+        int x1 = 3;
+        int y1 = 0;
+        int x2 = 6;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when02to62then6() {
+        double expected = 6;
+        int x1 = 0;
+        int y1 = 2;
+        int x2 = 6;
+        int y2 = 2;
         double out = Point.distance(x1, y1, x2, y2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
